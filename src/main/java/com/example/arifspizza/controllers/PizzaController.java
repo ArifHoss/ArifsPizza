22b460accab25affb062a01a60ea544113111606
Arif <hossaininfo@ymail.com>
2021-08-23 17:14:55 +0200

    DeleteMpping

--- src/main/java/com/example/arifspizza/controllers/PizzaController.java
+++ src/main/java/com/example/arifspizza/controllers/PizzaController.java
@@ -12,27 +12,38 @@ public class PizzaController {
 
     private final PizzaRepository pizzaRepository;
 
-    public PizzaController(PizzaRepository pizzaRepository){
-        this.pizzaRepository=pizzaRepository;
+    public PizzaController(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
     }
 
     @GetMapping("/pizzas")
-    public List<Pizza> pizzas(){
+    public List<Pizza> pizzas() {
         return pizzaRepository.findAll();
     }
 
     @GetMapping("/pizzas/{id}")
-    public Pizza get(@PathVariable Long id){
+    public Pizza get(@PathVariable Long id) {
         Optional<Pizza> pizza = pizzaRepository.findById(id);
-        if(pizza.isPresent()){
+        if (pizza.isPresent()) {
             return pizza.get();
-        }else{
-            throw new RuntimeException("There is no pizza found with this id: "+id+"! Try again with right Id.");
+        } else {
+            throw new RuntimeException("There is no pizza found with this id: " + id + "! Try again with right Id.");
         }
     }
 
     @PostMapping("/pizzas")
-    public Pizza addNewPizza(@RequestBody Pizza pizzaNew){
+    public Pizza addNewPizza(@RequestBody Pizza pizzaNew) {
         return pizzaRepository.save(pizzaNew);
+    }
+
+    @DeleteMapping("/pizzas/{id}")
+    public void removePizza(@PathVariable Long id) {
+        Optional<Pizza> pizza = pizzaRepository.findById(id);
+        if (pizza.isPresent()) {
+            pizzaRepository.deleteById(id);
+            System.out.println("Pizza with this id " + id + " has been successfully removed from list");
+        } else {
+            throw new RuntimeException("There is no pizza with this id: " + id);
+        }
     }
 }
