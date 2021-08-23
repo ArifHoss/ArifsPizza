07cc5098f2f090c9674123c97e0e5e638cd0add5
Arif <hossaininfo@ymail.com>
2021-08-23 11:32:48 +0200

    Initial commit. Entity class added. Application properties added.

---
+++ src/main/java/com/example/arifspizza/ArifsPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.arifspizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArifsPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArifsPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arifspizza/ArifsPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arifspizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArifsPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
