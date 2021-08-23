4cc59b1d148dcee0792045324e82709258bc517f
Arif <hossaininfo@ymail.com>
2021-08-23 12:17:11 +0200

    PizzaRepository class added

---
+++ src/main/java/com/example/arifspizza/repositories/PizzaRepository.java
@@ -0,0 +1,11 @@
+package com.example.arifspizza.repositories;
+
+import com.example.arifspizza.entities.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza,Long> {
+
+
+
+
+}
