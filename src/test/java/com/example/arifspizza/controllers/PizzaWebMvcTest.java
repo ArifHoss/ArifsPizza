7e5277bb8f12478edef5a902cf7e399322e236ef
Arif <hossaininfo@ymail.com>
2021-08-24 17:23:20 +0200

    added WebMvcTest

---
+++ src/test/java/com/example/arifspizza/controllers/PizzaWebMvcTest.java
@@ -0,0 +1,32 @@
+package com.example.arifspizza.controllers;
+
+import com.example.arifspizza.repositories.PizzaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = {PizzaController.class})
+@Import({})
+class PizzaWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PizzaRepository pizzaRepository;
+
+    @Test
+    void getAllPizzaReturnOnePizza() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/pizzas"))
+                .andExpect(status().is(200));
+
+    }
+
+}
